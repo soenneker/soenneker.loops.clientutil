d4332d792a64d1d27deaa4a844ea7955fd745efc
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:21 -0500

    TUnit conversion

--- test/Soenneker.Loops.ClientUtil.Tests/LoopsClientUtilTests.cs
+++ test/Soenneker.Loops.ClientUtil.Tests/LoopsClientUtilTests.cs
@@ -1,20 +1,19 @@
-﻿using Soenneker.Loops.ClientUtil.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Loops.ClientUtil.Abstract;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Loops.ClientUtil.Tests;
 
-[Collection("Collection")]
-public class LoopsClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class LoopsClientUtilTests : HostedUnitTest
 {
     private readonly ILoopsClientUtil _util;
 
-    public LoopsClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public LoopsClientUtilTests(Host host) : base(host)
     {
         _util = Resolve<ILoopsClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
